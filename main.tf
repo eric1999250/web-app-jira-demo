cc22eeb02bca4b4aef94d0f8896fd6c05e8e5224
eric <ericuwinezastarboy@gmail.com>
2025-12-05 16:58:20 +0200

    WEB-1: Initial frontend setup #done

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+}
+
+provider "local" {}
+
+locals {
+  app_code = <<-EOT
+    console.log("Terraform created this file!");
+  EOT
+}
+
+resource "local_file" "webapp_app" {
+  content  = local.app_code
+  filename = "${path.module}/webapp/src/index.js"
+}
+
+output "webapp_file_path" {
+  value       = resource.local_file.webapp_app.filename
+  description = "The full path to the web application's index.js file."
+}
---
+++ webapp/src/index.js
@@ -0,0 +1,1 @@
+console.log("Terraform created this file!");
